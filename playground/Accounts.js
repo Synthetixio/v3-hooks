be51a3192eeda6ab68757b2a5c92a2576ae97ebb
Noisekit <nikita@cc.snxdao.io>
2024-02-21 10:01:18 +0800

    useAccountOwner

--- playground/Accounts.js
+++ playground/Accounts.js
@@ -1,25 +1,59 @@
 import * as React from 'react';
 import { useAccounts } from '../lib/useAccounts';
+import { useAccountOwner } from '../lib/useAccountOwner';
 import { useSynthetix } from '../lib/useSynthetix';
 
-export function AccountsUI({ isLoading, data, isError, error }) {
+function QueryResult({ isError, error, isLoading, children }) {
+  if (isLoading) {
+    return React.createElement(React.Fragment, {}, 'Loading...');
+  }
+
+  if (isError) {
+    return React.createElement(
+      React.Fragment,
+      {},
+      error ? React.createElement('pre', {}, error.stack) : 'Error'
+    );
+  }
+
+  return React.createElement(React.Fragment, {}, children);
+}
+
+export function Account({ accountId }) {
+  const accountOwner = useAccountOwner({
+    accountId,
+  });
+
   return React.createElement(
-    'div',
+    'p',
     {},
-    isLoading
-      ? 'loading...'
-      : isError
-        ? `Error: ${error}`
-        : data && data.length > 0
-          ? data.map((account) => React.createElement('p', { key: account }, account))
-          : `No accounts`
+    'Account ID:',
+    ' ',
+    accountId,
+    ' ',
+    'Owner:',
+    ' ',
+    React.createElement(QueryResult, accountOwner, accountOwner.data)
   );
 }
 
 export function Accounts() {
   const [synthetix] = useSynthetix();
-  const { isLoading, data, isError, error } = useAccounts({
+  const accounts = useAccounts({
     walletAddress: synthetix.walletAddress,
   });
-  return React.createElement(AccountsUI, { isLoading, data, isError, error });
+
+  return React.createElement(
+    QueryResult,
+    accounts,
+    accounts.data && accounts.data.length > 0
+      ? React.createElement(
+          React.Fragment,
+          {},
+          accounts.data.map((accountId) =>
+            React.createElement(Account, { key: accountId, accountId })
+          )
+        )
+      : React.createElement(React.Fragment, {}, 'No accounts')
+  );
 }
