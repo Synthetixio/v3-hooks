a65205cb19f8fa6243acdfc1077259a7a0c425b6
Noisekit <nikita@cc.snxdao.io>
2024-02-27 11:23:19 +0800

    Fix test for Not Connected wallet

--- cypress/e2e/Home - Not Connected.e2e.js
+++ cypress/e2e/Home - Not Connected.e2e.js
@@ -2,5 +2,5 @@ it('shows homepage to not connected wallet', () => {
   cy.viewport(1100, 900);
   cy.visit('/');
   cy.get('#app').should('contain', 'Synthetix V3 Hooks Playground');
-  cy.get('#app').should('contain', 'Connect your wallet');
+  cy.contains('#app button', 'Connect');
 });
